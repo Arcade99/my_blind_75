a63503ca7221f795d22023656c6ba8b8f50f8604
Arcade99 <arcade99@proton.me>
2023-04-12 11:45:08 -0700

    improved valid_palindrome solution

--- two_pointers/valid_palindrome/solution.h
+++ two_pointers/valid_palindrome/solution.h
@@ -5,32 +5,25 @@
 class Solution {
 public:
     bool isPalindrome(std::string s) {
-        s = removeNonAlpha(s);
         int i = 0, j = (s.size() - 1);
         while (i < j) {
-            if (s[i] != s[j])
-            return false;
+            while (i < j && !isAlphaNumeric(s[i])) {
+                i++;
+            }
+            while (i < j && !isAlphaNumeric(s[j])) {
+                j--;
+            }
+            if (tolower(s[i]) != tolower(s[j])) {
+                return false;
+            }
             i++;
             j--;
         }
         return true;
     }
 
-    std::string removeNonAlpha(const std::string& s) {
-        std::string str = "";
-        for (char c : s) {
-            if (isAlpha(c)) {
-                if (isCapital(c)) {
-                    c += 32;
-                }
-                str += c;
-            }
-        }
-        return str;
-    }
-
-    bool isAlpha(char c) {
-        return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || (c >= 48 && c <= 57);
+    bool isAlphaNumeric(char c) {
+        return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || (c >= '0' && c <= '9');
     }
 
     bool isCapital(char c) {
