ceb13b6be249b2ca052b36726d6457cb410f440c
Arcade99 <arcade99@proton.me>
2023-04-13 00:04:11 -0700

    added solution for top_k_frequent_elements problem

---
+++ arrays_and_hashing/top_k_frequent_elements/solution.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <vector>
+#include <unordered_map>
+#include <queue>
+
+class Solution {
+public:
+    std::vector<int> topKFrequent(std::vector<int>& nums, int k) {
+        std::unordered_map<int, int> freqMap; // replacing with std::map yields better memory results, but worse run-time
+        for (int& i : nums) {
+            freqMap[i]++;
+        }
+        std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, myComparator> pq;
+        for (auto& p : freqMap) {
+            pq.emplace(p);
+        }
+        std::vector<int> results;
+        results.reserve(k);
+        for (unsigned i = 0; i < k; i++) {
+            results.emplace_back((pq.top()).first);
+            pq.pop();
+        }
+        return results;
+    }
+
+    struct myComparator {
+        bool operator()(const std::pair<int, int>& l, const std::pair<int, int>& r) const {
+            return l.second < r.second;
+        }
+    };
+};
