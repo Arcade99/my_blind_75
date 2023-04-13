af2da648d8f9c4e3294b964af49ed3757f218f35
Arcade99 <arcade99@proton.me>
2023-04-12 22:44:33 -0700

    added solution for group_anagrams

--- arrays_and_hashing/group_anagrams/solution.h
+++ arrays_and_hashing/group_anagrams/solution.h
@@ -2,45 +2,30 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
-#include <iostream>
 
 class Solution {
 public:
     std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs) {
-        
+        std::unordered_map<std::string, std::vector<std::string>> anagram_groups;
+        for (const std::string& s : strs) {
+            anagram_groups[myHash(s)].push_back(s);
+        }
+        std::vector<std::vector<std::string>> result;
+        for (auto& p : anagram_groups) {
+            result.push_back(p.second);
+        }
+        return result;
     }
 
-    std::vector<std::vector<std::string>> myGroupAnagrams(std::vector<std::string>& strs) {
-        std::unordered_map<int, std::vector<std::string>> myMap;
-        for (const std::string& str : strs) {
-            myMap[contentHash(str)].push_back(str);
+    std::string myHash(const std::string& s) {
+        std::vector<int> t(26, 0);
+        for (const char& c : s) {
+            t[c - 'a']++;
         }
-        std::vector<std::vector<std::string>> lists;
-        int x = 0;
-        for (std::pair<int, std::vector<std::string>> p : myMap) {
-            x++;
-            lists.push_back(p.second);
+        std::string str = "";
+        for (unsigned i = 0; i < 26; i++) {
+            str += std::to_string(t[i]) + ',';
         }
-        std::cout << x << std::endl;
-        return lists;
-    }
-
-    bool isAnagram(std::string s, std::string t) {
-        if (s.size() != t.size()) return false;
-        std::unordered_map<char, int> sFreq, tFreq;
-        for (unsigned i = 0; i < s.size(); i++) {
-            sFreq[s[i]]++;
-            tFreq[t[i]]++;
-        }
-        return sFreq == tFreq;
-    }
-
-    int contentHash(std::string s) {
-        int h = 0;
-        for (char& c : s) {
-            h += static_cast<int>(c);
-        }
-        std::cout << h << std::endl;
-        return h;
+        return str;
     }
 };
